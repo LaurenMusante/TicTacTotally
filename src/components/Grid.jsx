50d4ab2f2ab76390bd8e1928764208444892f2ca
Student <example@example.com>
2019-12-04 13:44:33 -0800

    Write click listener on squares

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -6,19 +6,19 @@ function Grid () {
     return (
         <div>
             <div className="row">
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square id='1'/></div>
+            <div className="col s1"><Square id='2'/></div>
+            <div className="col s1"><Square id='3'/></div>
             </div>
             <div className="row">
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square id='4'/></div>
+            <div className="col s1"><Square id='5'/></div>
+            <div className="col s1"><Square id='6'/></div>
             </div>
             <div className="row">
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
-            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square id='7'/></div>
+            <div className="col s1"><Square id='8'/></div>
+            <div className="col s1"><Square id='9'/></div>
             </div>
         </div>
     )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props)
+        this.handleClickingEmptySquare= this.handleClickingEmptySquare.bind(this);
+    }
+    
+    handleClickingEmptySquare(){
+        console.log(this.props.id);
+    }
+    
+        
+    
     render() {
+
+        var conditionalRender;
+        if(0==1){
+           conditionalRender=  'X';
+        }
+        else if(0==2){
+            
+             conditionalRender= 'O'            }
+        else{     
+                conditionalRender =<button onClick={this.handleClickingEmptySquare} className="">HI</button>;
+        }
+        
         return (
-            <div>
-            <button className="btn"> hi
-            </button>
-        </div>
+            <div className="btn">
+            {conditionalRender}
+            </div>
     );
     }
-}
+};
 
 export default Square;
