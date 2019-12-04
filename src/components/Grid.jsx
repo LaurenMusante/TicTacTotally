4e91e51a99e8423d6f14cc27b461e9fc884703bd
Student <example@example.com>
2019-12-04 11:01:45 -0800

    add more buttons to grid

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Square from './Square';
+import 'materialize-css/dist/css/materialize.min.css';
 
 function Grid () {
     return (
         <div>
-            <Square />
+            <div className="row">
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            </div>
+            <div className="row">
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            </div>
+            <div className="row">
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            <div className="col s1"><Square /></div>
+            </div>
         </div>
     )
 }
